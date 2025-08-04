43cabf6a357c123dcc3f1b33d8c22fae577de271
AntonioMuzzolini <antonio.muzzolini@techstar.it>
2025-08-04 17:41:51 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import DnDAudioManager from './components/DnDAudioManager'
+import './index.css'
+
+function App() {
+  return (
+    <div className="App">
+      <DnDAudioManager />
+    </div>
+  )
+}
+
+export default App
